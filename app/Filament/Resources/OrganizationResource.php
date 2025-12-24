730584a39e642cbea235d46614b3580a3e563232
Rafig Mojtahedzadeh Khiyavi <rafig_256@yahoo.com>
2025-12-24 22:10:59 +0330

    add organization crud page

---
+++ app/Filament/Resources/OrganizationResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OrganizationResource\Pages;
+use App\Filament\Resources\OrganizationResource\RelationManagers;
+use App\Models\Organization;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrganizationResource extends Resource
+{
+    protected static ?string $model = Organization::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-office-2';
+    protected static ?string $modelLabel = 'سازمان';
+    protected static ?string $pluralLabel = 'سازمان‌ها';
+    protected static ?string $navigationLabel = 'سازمان‌ها';
+    protected static ?int $navigationSort = 1;
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('logo_path')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('website')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('mobile')
+                    ->maxLength(10),
+                Forms\Components\Textarea::make('address')
+                    ->columnSpanFull(),
+                Forms\Components\Toggle::make('is_active')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('logo_path')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('website')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('mobile')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            RelationManagers\UsersRelationManager::class,
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrganizations::route('/'),
+            'create' => Pages\CreateOrganization::route('/create'),
+            'edit' => Pages\EditOrganization::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/OrganizationResource/RelationManagers/UsersRelationManager.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Resources\OrganizationResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UsersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'users';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('role')
+                ->label('نقش کاربر در سازمان'),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->label('نام کاربر'),
+                Tables\Columns\TextColumn::make('role')->label('نقش'), // نمایش نقش در جدول
+            ])
+            ->headerActions([
+                Tables\Actions\AttachAction::make() // برای متصل کردن کاربر موجود به سازمان
+                ->form(fn (Tables\Actions\AttachAction $action): array => [
+                    $action->getRecordSelect(),
+                    Forms\Components\TextInput::make('role')->label('نقش'), // مقداردهی پیوت موقع اتچ کردن
+                ]),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(), // ویرایش رکورد پیوت
+                Tables\Actions\DetachAction::make(), // جدا کردن کاربر از سازمان
+            ]);
+    }
+}
