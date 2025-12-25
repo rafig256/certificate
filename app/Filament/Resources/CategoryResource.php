9c9b94b9591ffe905b188e0851b785853243464f
Rafig Mojtahedzadeh Khiyavi <rafig_256@yahoo.com>
2025-12-25 23:59:56 +0330

    create categories filament resourse

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Filament\Resources\CategoryResource\RelationManagers;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'مدیریت گواهینامه';
+    protected static ?string $navigationLabel = 'دسته‌ها';
+    protected static ?string $pluralLabel = 'دسته‌ها';
+    protected static ?string $modelLabel = 'دسته';
+
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->label(__('fields.cat_name'))
+                    ->maxLength(255),
+                Forms\Components\Select::make('parent_id')
+                    ->label(__('fields.parent_cat'))
+                    ->relationship('parent', 'name')
+                    ->nullable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')->sortable()->label(__('fields.id')),
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable()->label(__('fields.cat_name')),
+                Tables\Columns\TextColumn::make('parent.name')->label(__('fields.parent_cat')),
+                Tables\Columns\TextColumn::make('created_at')->dateTime()->sortable()->label(__('fields.created_at')),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guarded = [];
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children(){
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }
