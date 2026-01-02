e7f78e759fe614e7148687f11af89314ca78a7f7
Rafig Mojtahedzadeh Khiyavi <rafig_256@yahoo.com>
2026-01-03 00:24:13 +0330

    add new logo_path field into signatories table with migration

--- database/migrations/2025_12_23_170558_create_signatories_table.php
+++ database/migrations/2025_12_23_170558_create_signatories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique()->nullable();
             $table->string('phone')->nullable();
+            $table->string('logo_path')->unique()->nullable();
             $table->enum('type', [
                 'علمی',
                 'دولتی',
