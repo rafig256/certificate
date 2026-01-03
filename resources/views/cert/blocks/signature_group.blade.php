bed8a7a30b008d15635ed9f02e0cb2cd197639c7
Rafig Mojtahedzadeh Khiyavi <rafig_256@yahoo.com>
2026-01-03 20:41:36 +0330

    add signatories logo, responsible and sing into certificate

--- resources/views/cert/blocks/signature_group.blade.php
+++ resources/views/cert/blocks/signature_group.blade.php
@@ -5,10 +5,29 @@
 <div class="block block-signatures align-{{ $block->align }}">
     @foreach ($context['signatories']->take($max) as $signer)
         <div class="signature">
-            @if($signer->signature_path)
-                <img src="{{ asset($signer->signature_path) }}" alt="">
+
+            {{-- تصویر امضا (overlay) --}}
+            @if($signer->sign_path)
+                <img class="signature-overlay" src="{{ asset('storage/'.$signer->sign_path) }}" alt="signature">
             @endif
-            <div class="name">{{ $signer->name }}</div>
+
+            <div class="name">
+                {{ $signer->name }}
+                @if($signer->responsible)
+                    <br>
+                    <span>{{ $signer->responsible }}</span>
+                @endif
+            </div>
+
+            @if($signer->logo_path)
+                <img
+                    class="logo"
+                    src="{{ asset('storage/'.$signer->logo_path) }}"
+                    alt=""
+                >
+            @endif
+
         </div>
     @endforeach
 </div>
+
