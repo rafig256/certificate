d900a55b58794ed6f601406e3f326301189d8b3e
Rafig Mojtahedzadeh Khiyavi <rafig_256@yahoo.com>
2026-01-22 00:39:30 +0330

    use local js and css replace by cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{ config('app.name', 'سامانه صدور گواهینامه آنلاین') }}</title>
-{{--        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css" rel="stylesheet">--}}
+        <link href="{{asset('css/bootstrap.rtl.min.css')}}" rel="stylesheet">
 {{--        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">--}}
         <link rel="stylesheet" href="{{asset('css/welcome.css')}}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('storage/home/favicon.png') }}">
         <link rel="shortcut icon" href="{{ asset('storage/home/favicon.ico') }}">
         {{--slider--}}
-{{--        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">--}}
-{{--        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>--}}
+        <link rel="stylesheet" href="{{asset('css/swiper-bundle.min.css')}}">
+        <script src="{{asset('js/swiper-bundle.min.js')}}"></script>
 
     </head>
     <body>
@@ -409,7 +409,7 @@
         </div>
     </footer>
 
-{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>--}}
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
     <script>
         // Hero Slider
         let currentSlide = 0;
